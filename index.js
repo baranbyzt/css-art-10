ffb9c939f22aedf8f08791b4f56f83d2eedd8191
baran beyazıt <byztmain2121@gmail.com>
2022-01-18 22:20:53 +0300

    first version is finished

---
+++ index.js
@@ -0,0 +1,35 @@
+
+
+
+ 
+
+
+
+ $(document).ready(function(){
+
+    
+    $(".inner_item").hover(function(){
+        $(this).css({
+
+      
+alert("j")
+
+        });
+        }, function(){
+        $(this).css({
+
+        });
+      });
+
+
+  });
+
+
+
+
+/*
+  $(".content").css(function(){
+    "display":"flex"
+})
+
+*/
