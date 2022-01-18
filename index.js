116acf730e9d455131daa8c96d2b5b580716fb8c
baran beyazıt <byztmain2121@gmail.com>
2022-01-18 23:06:59 +0300

    proje is finished

--- index.js
+++ index.js
@@ -7,29 +7,159 @@
 
  $(document).ready(function(){
 
-    
-    $(".inner_item").hover(function(){
-        $(this).css({
+    $( ".inner_item_1" ).hover(
+        function() {
+        
+            $(".content_1").css({
+                "display":"flex"
+            });
 
-      
-alert("j")
+            $(".inner_item_1").css({
+                "border":"15px rgb(30, 32, 28) solid",
+                "transition-duration": "1s"
+            })
 
-        });
-        }, function(){
-        $(this).css({
+        }, function() {
+ 
+            $(".content_1").css({
+                "display":"none"
+            });
+            $(".inner_item_1").css({
+                "border":"0px rgb(30, 32, 28) solid"
+            })
 
-        });
-      });
+
+
+        }
+      );
+      $( ".inner_item_2" ).hover(
+        function() {
+        
+            $(".content_2").css({
+                "display":"flex"
+            });
+
+            $(".inner_item_2").css({
+                "border":"15px rgb(30, 32, 28) solid",
+                "transition-duration": "1s"
+            })
+
+        }, function() {
+ 
+            $(".content_2").css({
+                "display":"none"
+            });
+            $(".inner_item_2").css({
+                "border":"0px rgb(30, 32, 28) solid"
+            })
+
+
+
+        }
+      );
+      $( ".inner_item_3" ).hover(
+        function() {
+        
+            $(".content_3").css({
+                "display":"flex"
+            });
+
+            $(".inner_item_3").css({
+                "border":"15px rgb(30, 32, 28) solid",
+                "transition-duration": "1s"
+            })
+
+        }, function() {
+ 
+            $(".content_3").css({
+                "display":"none"
+            });
+            $(".inner_item_3").css({
+                "border":"0px rgb(30, 32, 28) solid"
+            })
+
+
+
+        }
+      );
+      $( ".inner_item_4" ).hover(
+        function() {
+        
+            $(".content_4").css({
+                "display":"flex"
+            });
+
+            $(".inner_item_4").css({
+                "border":"15px rgb(30, 32, 28) solid",
+                "transition-duration": "1s"
+            })
+
+        }, function() {
+ 
+            $(".content_4").css({
+                "display":"none"
+            });
+            $(".inner_item_4").css({
+                "border":"0px rgb(30, 32, 28) solid"
+            })
+
+
+
+        }
+      );
+      $( ".inner_item_5" ).hover(
+        function() {
+        
+            $(".content_5").css({
+                "display":"flex"
+            });
+
+            $(".inner_item_5").css({
+                "border":"15px rgb(30, 32, 28) solid",
+                "transition-duration": "1s"
+            })
+
+        }, function() {
+ 
+            $(".content_5").css({
+                "display":"none"
+            });
+            $(".inner_item_5").css({
+                "border":"0px rgb(30, 32, 28) solid"
+            })
+
+
+
+        }
+      );
+      $( ".inner_item_6" ).hover(
+        function() {
+        
+            $(".content_6").css({
+                "display":"flex"
+            });
+
+            $(".inner_item_6").css({
+                "border":"15px rgb(30, 32, 28) solid",
+                "transition-duration": "1s"
+            })
+
+        }, function() {
+ 
+            $(".content_6").css({
+                "display":"none"
+            });
+            $(".inner_item_6").css({
+                "border":"0px rgb(30, 32, 28) solid"
+            })
+
+
+
+        }
+      );
 
 
   });
 
 
 
-
-/*
-  $(".content").css(function(){
-    "display":"flex"
-})
-
-*/
